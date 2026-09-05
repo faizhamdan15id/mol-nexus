fb0615922a27d440c2316933c75420325bd02f97
faizhamdan15id <faiztrueblues@gmail.com>
2026-09-06 04:55:10 +0700

    Update app.js

--- app.js
+++ app.js
@@ -307,8 +307,13 @@ async function joinNexus() {
 
 
         if (error) {
-            throw error;
-        }
+  alert(
+    "DEBUG JOIN ERROR:\n" +
+    JSON.stringify(error, null, 2)
+  );
+
+  throw error;
+}
 
 
         if (!data || data.length === 0) {
