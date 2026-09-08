ef1328056a3ed79a204954337336addd94363340
faizhamdan15id <faiztrueblues@gmail.com>
2026-09-08 16:47:59 +0700

    Update formula-bank.js

--- formula-bank.js
+++ formula-bank.js
@@ -198,7 +198,7 @@ formulaForm.addEventListener("submit", async (event) => {
 
   const formulaId =
   document.getElementById("formulaId").value;
-
+alert("FORMULA ID = " + formulaId);
 let result;
 
 if (formulaId) {
